e067d7139ba53ebf5cd253b17d0c283fcb4117ff
nmoneg <38598859+nmoneg@users.noreply.github.com>
2022-03-17 13:04:21 -0500

    Modified size input selection in designs.js

--- designs.js
+++ designs.js
@@ -1,11 +1,11 @@
 // Select color input
 const color = document.querySelector("input['#colorPicker']")
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+// Select size input
 const height = document.querySelector("input[name='height']");
 const width = document.querySelector("input[name='width']");
 
+// When size is submitted by the user, call makeGrid()
 document.addEventListener("submit", makeGrid());
 
 function makeGrid() {
