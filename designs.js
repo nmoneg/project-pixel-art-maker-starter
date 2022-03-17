92218387944c9d3d47f21535bd28324bb6bd4da8
nmoneg <38598859+nmoneg@users.noreply.github.com>
2022-03-17 12:56:47 -0500

    submit event listener added to designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,17 @@
 // Select color input
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const height = document.querySelector("input[name='height']");
+const width = document.querySelector("input[name='width']");
+
+document.addEventListener("submit", makeGrid());
 
 function makeGrid() {
 
 // Your code goes here!
+
+
 
 }
