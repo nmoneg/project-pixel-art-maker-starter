f786306ce880e6817accc2279093744621ba1bd4
nmoneg <38598859+nmoneg@users.noreply.github.com>
2022-03-18 18:01:42 -0500

    Modified variables in designs.js

--- designs.js
+++ designs.js
@@ -1,9 +1,9 @@
 // Select color input
-const color = document.querySelector("input['#colorPicker']")
+const color = document.querySelector("#colorPicker").value;
 
 // Select size input
-const height = document.querySelector("input[name='height']");
-const width = document.querySelector("input[name='width']");
+const height = document.querySelector("#inputHeight").value;
+const width = document.querySelector("#inputWidth").value;
 
 // When size is submitted by the user, call makeGrid()
 document.addEventListener("submit", makeGrid());
