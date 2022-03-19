5f7e34c5137cdd70ad7abf8f37835e4667101306
nmoneg <38598859+nmoneg@users.noreply.github.com>
2022-03-18 21:03:24 -0500

    Base grid function created in designs.js

--- designs.js
+++ designs.js
@@ -6,12 +6,23 @@ const height = document.querySelector("#inputHeight").value;
 const width = document.querySelector("#inputWidth").value;
 
 // When size is submitted by the user, call makeGrid()
-document.addEventListener("submit", makeGrid());
+const form = document.getElementById("sizePicker");
+// console.log("Form" + form.length);
+// console.log(JSON.stringify(form));
+form.addEventListener("submit", makeGrid(height, width));
 
-function makeGrid() {
+function makeGrid(x, y) {
 
 // Your code goes here!
+    let grid = document.createElement("table");
+    let oldtable = document.querySelector("#pixelCanvas");
 
+    for (let i = 0; i < x; i++) {
+        let gridRow = grid.insertRow(i);
+        for (let j = 0; j < y; j++) {
+            let gridCell = gridRow.insertCell(j);
+        }
+    }
 
-
+    oldtable.replaceWith(grid);
 }
