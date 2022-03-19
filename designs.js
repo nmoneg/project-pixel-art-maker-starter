8087196812b8918a4756b6f07eb8344645c4282c
nmoneg <38598859+nmoneg@users.noreply.github.com>
2022-03-18 22:44:00 -0500

    Grid function modified to clear previous table and trigger on submit

--- designs.js
+++ designs.js
@@ -1,22 +1,35 @@
 // Select color input
-const color = document.querySelector("#colorPicker").value;
+let color = document.querySelector("#colorPicker").value;
 
 // Select size input
-const height = document.querySelector("#inputHeight").value;
-const width = document.querySelector("#inputWidth").value;
+// let height = document.querySelector("#inputHeight").value;
+// let width = document.querySelector("#inputWidth").value;
 
 // When size is submitted by the user, call makeGrid()
 const form = document.getElementById("sizePicker");
 // console.log("Form" + form.length);
 // console.log(JSON.stringify(form));
-form.addEventListener("submit", makeGrid(height, width));
 
-function makeGrid(x, y) {
+form.addEventListener("submit", function(e) {
 
+    e.preventDefault();
+
+    let table = document.querySelector("#pixelCanvas");
+    if (table.hasChildNodes()) {
+        while (table.firstChild) {
+            table.removeChild(document.querySelector("tbody"));
+          }
+    }
+    makeGrid();
+});
+
+function makeGrid() {
 // Your code goes here!
-    let grid = document.createElement("table");
-    let oldtable = document.querySelector("#pixelCanvas");
-
+    let x = document.querySelector("#inputHeight").value;
+    let y = document.querySelector("#inputWidth").value;
+    
+    let grid = document.querySelector("#pixelCanvas");
+    
     for (let i = 0; i < x; i++) {
         let gridRow = grid.insertRow(i);
         for (let j = 0; j < y; j++) {
@@ -24,5 +37,4 @@ function makeGrid(x, y) {
         }
     }
 
-    oldtable.replaceWith(grid);
 }
